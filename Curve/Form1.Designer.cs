05bf5bd111a0291232a6819518e0e06311b86996
devgis <devgis@163.com>
2018-12-04 16:28:16 +0800

    add
    add

---
+++ Curve/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Curve
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.radTrackBar1 = new Telerik.WinControls.UI.RadTrackBar();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.radTrackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.zedGraphControl1.Location = new System.Drawing.Point(1, 0);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(731, 408);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.zedGraphControl1_Paint);
+            this.zedGraphControl1.MouseEnter += new System.EventHandler(this.zedGraphControl1_MouseEnter);
+            this.zedGraphControl1.MouseLeave += new System.EventHandler(this.zedGraphControl1_MouseLeave);
+            this.zedGraphControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.zedGraphControl1_MouseMove);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(1, 440);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "暂停(&S)";
+            this.toolTip1.SetToolTip(this.button1, "暂停并锁定监视");
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radTrackBar1
+            // 
+            this.radTrackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.radTrackBar1.BackColor = System.Drawing.Color.Transparent;
+            this.radTrackBar1.ForeColor = System.Drawing.Color.Transparent;
+            this.radTrackBar1.Location = new System.Drawing.Point(3, 411);
+            this.radTrackBar1.Name = "radTrackBar1";
+            this.radTrackBar1.Size = new System.Drawing.Size(726, 27);
+            this.radTrackBar1.TabIndex = 4;
+            this.radTrackBar1.Text = "radTrackBar1";
+            this.radTrackBar1.TickStyle = Telerik.WinControls.Enumerations.TickStyles.TopLeft;
+            this.radTrackBar1.TrackBarMode = Telerik.WinControls.UI.TrackBarRangeMode.Range;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(63, 440);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 27);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "打开(&O)";
+            this.toolTip1.SetToolTip(this.button2, "打开测点表");
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column4,
+            this.Column3,
+            this.Column5,
+            this.Column6});
+            this.dataGridView1.Location = new System.Drawing.Point(327, 244);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(400, 162);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.Visible = false;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "代号";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "名称";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column4.HeaderText = "原码";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column3.HeaderText = "工程值";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column5.HeaderText = "最大值";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column6.HeaderText = "最小值";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(140, 445);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 16);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "正常模式";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(233, 445);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(59, 16);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.Text = "百分比";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 469);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.radTrackBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "二次曲线";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.radTrackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private Telerik.WinControls.UI.RadTrackBar radTrackBar1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+
+
+    }
+}
+
---
+++ Curve/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ZedGraph;
+using System.Threading;
+
+namespace Curve
+{
+    public partial class Form1 : Form
+    {
+        int xMax = 150;//x轴总共显示的点数
+        int xSpace = 10;//x空出的点数
+        int yMax = 50;
+        int yMin = -50;
+        int RefreshTime = 500; //刷新时间
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public delegate void doDarw();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = RefreshTime;
+            DrawLine();
+            timer1.Start();
+
+            System.Windows.Forms.Timer t2 = new System.Windows.Forms.Timer();
+            t2.Interval = 1;
+            t2.Tick += new EventHandler(t2_Tick);
+            t2.Start();
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows[0].Cells[0].Value = "参数1";
+            dataGridView1.Rows[0].Cells[1].Value = "Parm1";
+            dataGridView1.Rows[0].Cells[4].Value = double.MaxValue;
+            dataGridView1.Rows[0].Cells[5].Value = double.MinValue;
+        }
+
+        void t2_Tick(object sender, EventArgs e)
+        {
+            if (bDraw)
+            {
+                DrawCross();
+            }
+        }
+        LineItem myCurve;
+        GraphPane myPane;
+        private void DrawLine()
+        {
+            PointPairList list = new PointPairList();
+            myPane = zedGraphControl1.GraphPane;
+            myPane.Title.Text = "二次曲线测试";
+            myPane.Title.FontSpec.Size = 10;
+            myPane.XAxis.Scale.BaseTic = 50;
+            myPane.YAxis.Title.Text = "工程值";
+            myPane.YAxis.Title.FontSpec.Size = 8;
+            myPane.XAxis.Title.Text = "时间";
+            myPane.XAxis.Title.FontSpec.Size = 8;
+            myPane.XAxis.Type = AxisType.DateAsOrdinal;
+            myPane.XAxis.CrossAuto = true;
+            //myPane.XAxis.Scale.Max = 50;
+            myPane.XAxis.Scale.Max = xMax;
+            myPane.XAxis.Scale.FontSpec.Size = 5;
+            myPane.YAxis.Scale.Max = yMax;
+            myPane.YAxis.Scale.Min = yMin;
+            myPane.YAxis.Scale.FontSpec.Size = 5;
+
+            myPane.Legend.Border.Color = Color.DarkGray; //去掉图例边框
+            myPane.Legend.FontSpec.Size = 8; //图例字体大小
+
+            myPane.Legend.Position = ZedGraph.LegendPos.InsideTopLeft;
+            myPane.Legend.Location = new ZedGraph.Location(50, 50, CoordType.AxisXYScale);
+            
+            //myPane.XAxis.Scale.Min = 500;
+            myPane.XAxis.Scale.Format = "hh:mm:ss";
+            myCurve = myPane.AddCurve("参数1", list, Color.Red, SymbolType.None);
+            
+            myCurve.Line.IsAntiAlias = true;
+            myCurve.Line.Width = 1.5F;
+            myCurve.Symbol.Fill = new Fill(Color.White);
+            myCurve.Symbol.Size = 5;
+        }
+        int i = 0;
+        int iPos = 0;
+        Random rd = new Random();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            double x = (double)new XDate(DateTime.Now);
+            double y = Math.Sin(i * Math.PI / 15.0) * 16.0;
+
+            dataGridView1.Rows[0].Cells[2].Value = y*rd.NextDouble();
+            dataGridView1.Rows[0].Cells[3].Value = y;
+
+            if (y < Convert.ToDouble(dataGridView1.Rows[0].Cells[4].Value))
+            {
+                dataGridView1.Rows[0].Cells[4].Value = y;
+            }
+            if (y> Convert.ToDouble(dataGridView1.Rows[0].Cells[5].Value))
+            {
+                dataGridView1.Rows[0].Cells[5].Value = y;
+            }
+            //if (myCurve.Points.Count > 100)
+            //{
+            //    myCurve.RemovePoint(0);
+            //}
+            myCurve.AddPoint(x, y);
+
+            //double x1=0, y1=0;
+            myPane.ReverseTransform(new PointF((float)myPane.XAxis.Scale.Min,0f), out x, out y);
+            //XDate xd = new XDate(x1);
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Refresh();
+            //myPane.GetImage(true).Save(); //调用保存接口
+            i++;
+            
+            //radTrackBar1.Value = 0.5f * i;
+            radTrackBar1.Maximum = i;
+            //radTrackBar1.Value = i;
+            radTrackBar1.Ranges.Maximum =i;
+            this.radTrackBar1.Ranges[0].End = i;
+            try
+            {
+                
+                //radTrackBar1.Ranges.Maximum = 0.5f * i;
+                //radTrackBar1.Ranges.Maximum = 0.5f * i;
+                if (i > (xMax - xSpace))
+                {
+                    this.radTrackBar1.Ranges[0].Start = i - xMax + xSpace;
+                }
+                else
+                {
+                    this.radTrackBar1.Ranges[0].Start = 0;
+                }
+                
+                
+            }
+            catch
+            { }
+            if (iPos < xMax - xSpace)
+            {
+                iPos++;
+            }
+            else
+            {
+                myPane.XAxis.Scale.Min++;
+                myPane.XAxis.Scale.Max++;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text.Equals("暂停(&S)"))
+            {
+                button1.Text = "开始(&S)";
+                toolTip1.SetToolTip(button1, "开始监视");
+                timer1.Stop();
+            }
+            else
+            {
+                button1.Text = "暂停(&S)";
+                toolTip1.SetToolTip(button1,"暂停并锁定监视");
+                timer1.Start();
+            }
+        }
+
+        private void zedGraphControl1_MouseMove(object sender, MouseEventArgs e)
+        {
+            #region[曲线显示值例子]
+            PointF mousePt = new PointF(e.X, e.Y);
+            GraphPane mypane = ((ZedGraphControl)sender).MasterPane.FindChartRect(mousePt);
+            //StockPointList stockPointList = new StockPointList(stockChart.stockList);
+
+
+            if (point_x != MousePosition.X || point_y != MousePosition.Y)
+            {
+
+                if (mypane != null)
+                {
+
+                    point_x = MousePosition.X;
+                    point_y = MousePosition.Y;
+                    double x, y;
+                    //StockPt mouseStock;
+                    mypane.ReverseTransform(mousePt, out x, out y);
+                    //mouseStock = stockPointList.GetAt((int)x - 1);
+                    //toolTip1.SetToolTip(zedGraphControl1, "Open=" + mouseStock.Open.ToString("f2") + " \n High=" + mouseStock.High.ToString("f2") + "\n Low=" + mouseStock.Low.ToString("f2") + "\n Close=" + mouseStock.Close.ToString("f2"));
+
+
+
+                }
+                zedGraphControl1.Refresh();
+                currentPoint = new Point(e.X, e.Y);
+
+                DrawCross();
+            }
+
+
+            #endregion
+
+            //// Save the mouse location  
+            //PointF mousePt = new PointF(e.X, e.Y);
+            //dx = e.X;
+            //string InfoString = string.Empty;
+
+            //// Find the Chart rect that contains the current mouse location  
+            //GraphPane pane = ((ZedGraphControl)sender).MasterPane.FindChartRect(mousePt);
+
+            //// If pane is non-null, we have a valid location.  Otherwise, the mouse is not  
+            //// within any chart rect.  
+            //if (pane != null)
+            //{
+            //    double x, y;
+            //    // Convert the mouse location to X, and Y scale values  
+            //    pane.ReverseTransform(mousePt, out x, out y);
+            //    // 获取横纵坐标信息  
+            //    //x=myPane.XAxis.
+            //    //XDate xd = new XDate(x);
+            //    //DateTime date = xd.DateTime;
+            //    //InfoString = xd.ToString();
+            //}
+        }
+
+
+        private void DrowShuXian(double x)
+        {
+            //zedGraphControl1.getd
+        }
+
+        //Pen pPen;
+        //SolidBrush mysbrush = new SolidBrush(Color.Red);
+        public float point_x = 0.0F;
+        public float point_y = 0.0F;
+        private Point currentPoint = new Point(0, 0);    
+        private void zedGraphControl1_Paint(object sender, PaintEventArgs e)
+        {
+            if (bDraw)
+            {
+                DrawCross();
+            }
+            //Graphics g = e.Graphics;
+            //pPen = new Pen(mysbrush);
+            //g.DrawLine(pPen, new Point(100,100), new Point(100, 0));
+            //g.DrawLine(pPen, new Point(dx, zedGraphControl1.Height), new Point(dx, 0));
+        }
+
+        bool bDraw = false;
+        private void zedGraphControl1_MouseEnter(object sender, EventArgs e)
+        {
+            bDraw = true;
+        }
+
+        private void zedGraphControl1_MouseLeave(object sender, EventArgs e)
+        {
+            bDraw = false;
+        }
+
+        //绘制用于方便查看刻度的十字线
+        private void DrawCross()
+        {
+            Graphics g = zedGraphControl1.CreateGraphics();
+            g.DrawLine(Pens.Green, 0, currentPoint.Y, this.Width, currentPoint.Y); //绘制横线  
+            g.DrawLine(Pens.Green, currentPoint.X, 0, currentPoint.X, this.Height); //会制纵线 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text.Equals("打开(&O)"))
+            {
+                button2.Text = "关闭(&C)";
+                toolTip1.SetToolTip(button2, "关闭测点表");
+                dataGridView1.Visible = true;
+            }
+            else
+            {
+                button2.Text = "打开(&O)";
+                toolTip1.SetToolTip(button2, "打开测点表");
+                dataGridView1.Visible = false;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                myPane.YAxis.Scale.Max = yMax;
+                myPane.YAxis.Scale.Min = yMin;
+            }
+            else
+            {
+                myPane.YAxis.Scale.Max = 100;
+                myPane.YAxis.Scale.Min = 0;
+            }
+        }
+
+    }
+}
